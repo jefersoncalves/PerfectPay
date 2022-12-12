839649a2a5a2b0159e07081446ee96ed80c97f99
Jeferson Alves <jefersoncamposalves@icloud.com>
2022-12-12 13:57:24 -0300

    Final project

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PayApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
+
---
+++ MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿namespace PayApp;
+
+public partial class MainPage : ContentPage
+{
+	decimal bill;
+	int tip;
+	int noPerson = 1;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    void txtBill_Completed(System.Object sender, System.EventArgs e)
+    {
+		bill = decimal.Parse(txtBill.Text);
+		CalculateTotal();
+    }
+
+    private void CalculateTotal()
+    {
+        // Total Tip
+        var totalTip = (bill * tip) / 100;
+
+
+        // Tip by Person
+        var tipByPerson = (totalTip / noPerson);
+        lblTipByPerson.Text = $"{tipByPerson:C}";
+
+        // Subtotal
+        var subtotal = (bill / noPerson);
+        lblSubtotal.Text = $"{subtotal:C}";
+
+        // Total
+        var totalByPerson = (bill + totalTip) / noPerson;
+        lblTotal.Text = $"{totalByPerson:C}";
+    }
+
+    void sldTip_ValueChanged(System.Object sender, Microsoft.Maui.Controls.ValueChangedEventArgs e)
+    {
+        tip = (int)sldTip.Value;
+        lblTip.Text = $"Tip: {tip}%";
+        CalculateTotal();
+    }
+
+    void Button_Clicked(System.Object sender, System.EventArgs e)
+    {
+        if(sender is Button)
+        {
+            var btn = (Button)sender;
+            var percentage = int.Parse(btn.Text.Replace("%", ""));
+            sldTip.Value = percentage;
+        }
+    }
+
+    void btnMinus_Clicked(System.Object sender, System.EventArgs e)
+    {
+        if(noPerson > 1)
+        {
+            noPerson--;
+        }
+        lblNoPerson.Text = noPerson.ToString();
+        CalculateTotal();
+    }
+
+    void btnPlus_Clicked(System.Object sender, System.EventArgs e)
+    {
+        noPerson++;
+        lblNoPerson.Text = noPerson.ToString();
+        CalculateTotal();
+    }
+}
+
+
